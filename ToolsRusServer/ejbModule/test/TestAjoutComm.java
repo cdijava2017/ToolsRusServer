8763fa227943dec4edebc8f67fbb9d3a4e10cd32
Nacer A <shindelys@gmail.com>
2018-04-23 16:57:06 +0200

    Commit fin de journée 23/04

--- ToolsRusServer/ejbModule/test/TestAjoutComm.java
+++ ToolsRusServer/ejbModule/test/TestAjoutComm.java
@@ -1,6 +1,7 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import javax.naming.Context;
@@ -27,11 +28,13 @@ public class TestAjoutComm {
 	private int nb = 1;
 	private static IFacade interfaceFacade;
 	private Commentaire commentaire1;
-//	private Commentaire commentaire2;
+	private Commentaire commentaireTest;
 //	private ArrayList<Commentaire> listeCommentaires = null;
 	private Titre titre1;
 	
 	/**
+	 * Au lancement de la classe de test, la méthode debutClasse() annotée @BeforeClass se lance (une seule fois)
+	 * et ouvre la connexion avec le serveur pour tous les tests qui vont être exécutés ensuite.
 	 * @throws java.lang.Exception
 	 */
 	@BeforeClass // 1 fois au lancement de la classe de test
@@ -46,12 +49,15 @@ public class TestAjoutComm {
 	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		
 	}
 
 	/**
+	 * Avant chaque cas de test, nous supprimons les commentaires en base pour commencer le test avec 
+	 * base saine et vide, ce qui évitera les erreurs de doublons si ce n'est pas voulu.
 	 * @throws java.lang.Exception
 	 */
-	@Before // avant chaque test, nous supprimons les commentaire en base
+	@Before 
 	public void debut() throws Exception { 
 		System.out.println("@Before : " + commentaire1);
 		interfaceFacade.supAllCompteurs();
@@ -62,16 +68,17 @@ public class TestAjoutComm {
 	/**
 	 * @throws java.lang.Exception
 	 */
-	@After // après chaque test, nous supprimons le contenu des tables commentaire, titre et compteur
+	@After 
 	public void fin() throws Exception { 
 		System.out.println("@After : " + commentaire1);
 
 	}
 
 	/**
+	 * Cas nominal! On créé un commentaire avec un idComm = 1, txtComm = "commentaire 1" et on lui associe un titre idTitre = 1, txtTitre = titre 1.
+	 * Ensuite, on persiste et on va chercher en base le commentaire pour vérifier qu'il existe bien
 	 * Test method for {@link dao.uc3_Donner1Avis.DaoGestion#ajouter(entity.uc3_Donner1Avis.commentaire.Commentaire)}.
 	 * @throws CommentaireException 
-	 * Cas nominal!
 	 */
 	@Test
 	public final void testAjouterCommCasNominal() throws CommentaireException {
@@ -88,11 +95,17 @@ public class TestAjoutComm {
 		} catch (CommentaireException e) {
 			System.out.println("exception testCreerComm() : " + e.getMessage());
 		}
-		if (interfaceFacade.getCommParRef(commentaire1.getIdComm()).equals(commentaire1)) retour = true;
+		commentaireTest = interfaceFacade.getCommParRef(commentaire1.getIdComm());
+		if (commentaireTest instanceof Commentaire && commentaire1.equals(commentaireTest) ) {
+			retour = true;
+		}
 		assertTrue(retour);
 	}
 
 	/**
+	 * On créé un commentaire null et on ne lui associe pas de titre. Ensuite, on persiste en base le commentaire. 
+	 * La persistence doit nous renvoyer une CommentaireException avec un message. Après avoir récupéré le message dans la variable @param obtenu,
+	 * on le compare avec celui stocké dans la variable @param UtilBdD.COMM_NULL. Si les deux messages sont les mêmes, on set @param retour à true,
 	 * Test method for {@link dao.uc3_Donner1Avis.DaoGestion#ajouter(entity.uc3_Donner1Avis.commentaire.Commentaire)}.
 	 * @throws CommentaireException 
 	 */
@@ -111,10 +124,12 @@ public class TestAjoutComm {
 			obtenu = e.getMessage();
 		} 
 		assertEquals(attendu, obtenu);
-		
 	}
 
 	/**
+	 * On créé un commentaire avec un id null (une variable de type int ne peut être vide, l'équivalent est le chiffre 0) 
+	 * et on ne lui associe pas de titre. Ensuite, on persiste en base le commentaire. 
+	 * La persistence doit nous renvoyer une CommentaireException avec un message stocké dans la variable @param IDCOMM_INVALIDE .
 	 * Test method for {@link dao.uc3_Donner1Avis.DaoGestion#ajouter(entity.uc3_Donner1Avis.commentaire.Commentaire)}.
 	 * @throws CommentaireException 
 	 */
@@ -136,6 +151,8 @@ public class TestAjoutComm {
 	}
 
 	/**
+	 * On créé un commentaire avec un idComm = 1 et un txtComm à null mais on ne lui associe pas de titre. Ensuite, on persiste en base le commentaire. 
+	 * La persistence doit nous renvoyer une CommentaireException avec un message stocké dans la variable @param TXTCOMM_INVALIDE .
 	 * Test method for {@link dao.uc3_Donner1Avis.DaoGestion#ajouter(entity.uc3_Donner1Avis.commentaire.Commentaire)}.
 	 * @throws CommentaireException 
 	 */
@@ -157,26 +174,26 @@ public class TestAjoutComm {
 	}
 
 	/**
+	 * On créé un commentaire avec un idComm = 1, txtComm = "commentaire 1" et on lui associe un titre idTitre = 1, txtTitre = titre 1.
+	 * La persistence doit nous renvoyer une CommentaireException avec un message stocké dans la variable @param ERREUR_2 .
 	 * Test method for {@link dao.uc3_Donner1Avis.DaoGestion#ajouter(entity.uc3_Donner1Avis.commentaire.Commentaire)}.
 	 * @throws CommentaireException 
 	 */
 	@Test
 	public final void testAjouterCommDoublon() throws CommentaireException {
-//		String attendu = UtilBdD.TXTCOMM_INVALIDE;
-//		String obtenu = null;
+		String attendu = UtilBdD.ERREUR_2;
+		String obtenu = null;
 		setCommentaire1(new Commentaire(nb, "commentaire " + nb));
 		setTitre1(new Titre(commentaire1.getIdComm(), "titre " + nb));
 		commentaire1.setTitre(titre1);
 		System.out.println("testAjouterCommDoublon() : " + commentaire1);
-		boolean retour = false;
 		// Première persistence pour ajouter le commentaire en base
 		try {
 			System.out.println("persistence commentaire");
 			interfaceFacade.addCommentaire(commentaire1);
 			System.out.println("commentaire persisté");
 		} catch (CommentaireException e) {
-			System.out.println("exception testAjouterCommDoublon() : " + e.getMessage());
-//			obtenu = e.getMessage();
+			System.out.println("1ère exception testAjouterCommDoublon() : " + e.getMessage());
 		} 
 		// Deuxième persistence pour créer un doublon
 		try {
@@ -184,10 +201,44 @@ public class TestAjoutComm {
 			interfaceFacade.addCommentaire(commentaire1);
 			System.out.println("commentaire persisté");
 		} catch (CommentaireException e) {
-			System.out.println("exception testAjouterCommDoublon() : " + e.getMessage());
-//			obtenu = e.getMessage();
+			System.out.println("2ème exception testAjouterCommDoublon() : " + e.getMessage());
+			obtenu = e.getMessage();
 		} 
-//		assertEquals(attendu, obtenu);
+		assertEquals(attendu, obtenu);
+	}
+	
+	/**
+	 * On créé un commentaire avec un idComm = 1, txtComm = "commentaire 1" et on lui associe un titre idTitre = 1, txtTitre = titre 1.
+	 * La persistence doit nous renvoyer une CommentaireException avec un message stocké dans la variable @param ERREUR_2 .
+	 * Test method for {@link dao.uc3_Donner1Avis.DaoGestion#ajouter(entity.uc3_Donner1Avis.commentaire.Commentaire)}.
+	 * @throws CommentaireException 
+	 */
+	@Test
+	public final void testAjouterCommTitreNull() throws CommentaireException {
+		String attendu = UtilBdD.ERREUR_2;
+		String obtenu = null;
+		setCommentaire1(new Commentaire(nb, "commentaire " + nb));
+		setTitre1(new Titre(commentaire1.getIdComm(), "titre " + nb));
+		commentaire1.setTitre(titre1);
+		System.out.println("testAjouterCommDoublon() : " + commentaire1);
+		// Première persistence pour ajouter le commentaire en base
+		try {
+			System.out.println("persistence commentaire");
+			interfaceFacade.addCommentaire(commentaire1);
+			System.out.println("commentaire persisté");
+		} catch (CommentaireException e) {
+			System.out.println("1ère exception testAjouterCommDoublon() : " + e.getMessage());
+		} 
+		// Deuxième persistence pour créer un doublon
+		try {
+			System.out.println("persistence commentaire");
+			interfaceFacade.addCommentaire(commentaire1);
+			System.out.println("commentaire persisté");
+		} catch (CommentaireException e) {
+			System.out.println("2ème exception testAjouterCommDoublon() : " + e.getMessage());
+			obtenu = e.getMessage();
+		} 
+		assertEquals(attendu, obtenu);
 	}
 	
 	public Commentaire getCommentaire1() {
